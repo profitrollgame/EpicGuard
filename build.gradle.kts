7636afe04ed66cab9d19c19ab56fdd4e499615ef
Adrian <adriangonzalesval@gmail.com>
2023-02-11 16:02:32 -0500

    build: improved default configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,43 +18,52 @@ allprojects {
 }
 
 subprojects {
-    tasks.withType<ShadowJar> {
-        val platformName = project.name.capitalize()
-        archiveFileName.set("EpicGuard$platformName-${project.version}.jar")
+    tasks {
+        withType<ShadowJar> {
+            val platformName = project.name.capitalize()
+            archiveFileName.set("EpicGuard$platformName-${project.version}.jar")
 
-        relocate("org.spongepowered.configurate", "me.xneox.epicguard.libs.configurate")
-        relocate("org.apache.commons", "me.xneox.epicguard.libs.apachecommons")
-        relocate("com.fasterxml", "me.xneox.epicguard.libs.fasterxml")
-        relocate("com.maxmind", "me.xneox.epicguard.libs.maxmind")
-        relocate("com.typesafe.config", "me.xneox.epicguard.libs.config")
-        relocate("com.zaxxer.hikari", "me.xneox.epicguard.libs.hikari")
-        relocate("io.leangen.geantyref", "me.xneox.epicguard.libs.geantyref")
+            relocate("org.spongepowered.configurate", "me.xneox.epicguard.libs.configurate")
+            relocate("org.apache.commons", "me.xneox.epicguard.libs.apachecommons")
+            relocate("com.fasterxml", "me.xneox.epicguard.libs.fasterxml")
+            relocate("com.maxmind", "me.xneox.epicguard.libs.maxmind")
+            relocate("com.typesafe.config", "me.xneox.epicguard.libs.config")
+            relocate("com.zaxxer.hikari", "me.xneox.epicguard.libs.hikari")
+            relocate("io.leangen.geantyref", "me.xneox.epicguard.libs.geantyref")
 
-        minimize()
+            minimize()
 
-        // Copy compiled platform jars to '/build' directory for convenience.
-        doLast {
-            copy {
-                from(archiveFile)
-                into("${rootProject.projectDir}/build")
+            // Copy compiled platform jars to '/build' directory for convenience.
+            if (project.name != "core") {
+                doLast {
+                    copy {
+                        from(archiveFile)
+                        into("${rootProject.projectDir}/build")
+                    }
+                }
             }
+
         }
-    }
 
-    tasks.withType<Delete> {
-        delete("run")
-    }
+        build {
+            dependsOn(shadowJar)
+        }
 
-    tasks.withType<JavaCompile> {
-        options.encoding = Charsets.UTF_8.name()
-        options.release.set(17)
-    }
+        withType<Delete> {
+            delete("run")
+        }
 
-    // TODO: replace with plugin.yml plugin to avoid deprecation warning
-    // For Waterfall and Paper platforms: set version
-    tasks.withType<ProcessResources> {
-        filesMatching("plugin.yml") {
-            expand("version" to project.version)
+        withType<JavaCompile> {
+            options.encoding = Charsets.UTF_8.name()
+            options.release.set(17)
+        }
+
+        // TODO: replace with plugin.yml plugin to avoid deprecation warning
+        // For Waterfall and Paper platforms: set version
+        withType<ProcessResources> {
+            filesMatching("plugin.yml") {
+                expand("version" to project.version)
+            }
         }
     }
 }
