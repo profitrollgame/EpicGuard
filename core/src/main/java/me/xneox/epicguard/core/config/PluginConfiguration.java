ac6fa1dff36f30bebda543abd1af835538ef22e4
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2023-06-26 19:03:51 +0200

    Fix default

--- core/src/main/java/me/xneox/epicguard/core/config/PluginConfiguration.java
+++ core/src/main/java/me/xneox/epicguard/core/config/PluginConfiguration.java
@@ -400,7 +400,7 @@ public class PluginConfiguration {
     private long attackResetInterval = 80L;
 
     @Comment("Disable IPs in tab completion.")
-    private boolean disableIPTabCompletion = true;
+    private boolean disableIPTabCompletion = false;
 
     @Comment("Set to false to disable update checker.")
     private boolean updateChecker = true;
